a11cc257b5ec2f460532ad6ea7b2052fc372dabc
Eugene Tkachenko <gektor650@gmail.com>
2024-06-16 21:07:32 -0600

    Add raspberry Zero support

--- raspberry/install_mediamtx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/fpv-lawn-mover/raspberry
-wget https://github.com/bluenviron/mediamtx/releases/download/v1.8.2/mediamtx_v1.8.2_linux_arm64v8.tar.gz
-tar -xvzf mediamtx_v1.8.2_linux_arm64v8.tar.gz
-rm mediamtx_v1.8.2_linux_arm64v8.tar.gz
-
-nano mediamtx.yml
